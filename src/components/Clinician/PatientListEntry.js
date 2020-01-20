05fba215871075b04659ede80c8001dd461faeb9
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-01-20 17:58:59 +0000

    Implemented backend for patient list

--- src/components/Clinician/PatientListEntry.js
+++ src/components/Clinician/PatientListEntry.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
 function PatientListEntry(props) {
-    return (
-        <tr>
-            <td>{props.id}</td>
-            <td><a href={"PatientRecords?id=" + props.id + "&name=" + props.name + "&patientno=" + props.patientNumber
-             + "&birthday=" + props.birthday + "&reason=" + props.ailments}>{props.name}</a></td>
-            <td>{props.patientNumber}</td>
-            <td>{props.birthday}</td>
-            <td>{props.date}</td>
-            <td>{props.patientType}</td>
-            <td>{props.ailments}</td>
-        </tr>
-    );
+  return (
+    <tr>
+      {/*<td>{props.id}</td>*/}
+      {/*<td><a href={"PatientRecords?id=" + props.id + "&name=" + props.name + "&patientno=" + props.patientNumber*/}
+      {/* + "&birthday=" + props.birthday + "&reason=" + props.ailments}>{props.name}</a></td>*/}
+      {/*<td>{props.patientNumber}</td>*/}
+      {/*<td>{props.birthday}</td>*/}
+      {/*<td>{props.date}</td>*/}
+      {/*<td>{props.patientType}</td>*/}
+      {/*<td>{props.ailments}</td>*/}
+      <td>{props.id}</td>
+      <td><a href={"PatientRecords?id=" + props.id + "&name=" + props.patientId + "&patientno=" + props.nhsNumber
+       + "&birthday=1995-10-11&reason=Lurgy"}>{props.patientId}</a></td>
+      <td>{props.gender}</td>
+      <td>{props.sex}</td>
+      <td>{props.vitalStatus}</td>
+      <td>{props.birthYear}</td>
+      <td>{props.nhsNumber}</td>
+      <td>{props.timeCreated}</td>
+    </tr>
+  );
 }
 
 export default PatientListEntry;
--- src/views/Clinician/PatientList.js
+++ src/views/Clinician/PatientList.js
@@ -3,6 +3,45 @@ import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import HeaderMenu from "../../components/HeaderMenu";
 import PatientListEntry from "../../components/Clinician/PatientListEntry";
+import getEHRs from "../../components/GetEHRs";
+
+class PatientListTable extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		let promise = getEHRs();
+		promise.then((e) => {
+			this.setState({ ehrs: e });
+		});
+	}
+
+	render () {
+		if (!this.state.ehrs) return null;
+		return <Table striped bordered hover>
+			<thead>
+			<tr>
+				<th>#</th>
+				<th>Patient ID</th>
+				<th>Gender</th>
+				<th>Sex</th>
+				<th>Vital Status</th>
+				<th>Birth Year</th>
+				<th>NHS Number</th>
+				<th>Time created</th>
+			</tr>
+			</thead>
+			<tbody>
+			{this.state.ehrs.map((e, index) => {
+				e.id = index + 1;
+				return PatientListEntry(e);
+			})}
+			</tbody>
+		</Table>;
+	}
+}
 
 function PatientList() {
 	return (
@@ -12,66 +51,7 @@ function PatientList() {
 				<div style={{ height: '50px' }}></div>
 				<div style={{ display: 'block', textAlign: 'center' }}>
 					<div><h1>Patient List</h1></div>
-					<Table striped bordered hover>
-						<thead>
-						<tr>
-							<th>#</th>
-							<th>Name</th>
-							<th>Patient Number</th>
-							<th>Birthday</th>
-							<th>Date</th>
-							<th>Patient Type</th>
-							<th>Comments</th>
-						</tr>
-						</thead>
-						<tbody>
-						<PatientListEntry id={"1"} name={"Mark"} patientNumber={"800032"} birthday={"19750324"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@mdo"}/>
-						<PatientListEntry id={"2"} name={"Jacob"} patientNumber={"800033"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"3"} name={"Jacob"} patientNumber={"800034"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"4"} name={"Jacob"} patientNumber={"800035"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"5"} name={"Jacob"} patientNumber={"800036"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"6"} name={"Jacob"} patientNumber={"800037"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"7"} name={"Jacob"} patientNumber={"800038"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"Out-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"8"} name={"Jacob"} patientNumber={"800039"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"9"} name={"Jacob"} patientNumber={"800040"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"Out-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"10"} name={"Jacob"} patientNumber={"800041"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"11"} name={"Jacob"} patientNumber={"800042"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"12"} name={"Jacob"} patientNumber={"800043"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"13"} name={"Jacob"} patientNumber={"800044"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"Out-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"14"} name={"Jacob"} patientNumber={"800045"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						<PatientListEntry id={"15"} name={"Jacob"} patientNumber={"800046"} birthday={"19870608"}
-										  date={"19/10/2019"}
-										  patientType={"In-Patient"} ailments={"@fat"}/>
-						</tbody>
-					</Table>
+					<PatientListTable />
 				</div>
 			</Container>
 
