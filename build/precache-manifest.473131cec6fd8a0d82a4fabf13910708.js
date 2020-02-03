f51f10847c4c1112ffe910633fc1de1ff4b9c0ee
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-02-03 16:04:09 +0000

    Removed unneeded build files

---
+++ build/precache-manifest.473131cec6fd8a0d82a4fabf13910708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1469a5889bb76892a1dfd2f73d995d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d731b2adf4ee5fd9f39",
+    "url": "/static/js/2.4daf457d.chunk.js"
+  },
+  {
+    "revision": "7e7e842cba242700f8801c2bacf7f60c",
+    "url": "/static/js/2.4daf457d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0abf0d4efb555a8d68a7",
+    "url": "/static/js/main.a86ea2be.chunk.js"
+  },
+  {
+    "revision": "cd5f09d757ce3582c9e8",
+    "url": "/static/js/runtime-main.9ce0839b.js"
+  }
+]);
