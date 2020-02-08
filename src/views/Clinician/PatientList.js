92dbcecb2c649bfd174c3379f4d388d0a091d8e5
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-02-08 15:13:24 +0000

    Reindenting

--- src/views/Clinician/PatientList.js
+++ src/views/Clinician/PatientList.js
@@ -6,57 +6,57 @@ import PatientListEntry from "../../components/Clinician/PatientListEntry";
 import getEHRs from "../../components/GetEHRs";
 
 class PatientListTable extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-	componentDidMount() {
-		let promise = getEHRs();
-		promise.then((e) => {
-			this.setState({ ehrs: e });
-		});
-	}
+    componentDidMount() {
+        let promise = getEHRs();
+        promise.then((e) => {
+            this.setState({ ehrs: e });
+        });
+    }
 
-	render () {
-		if (!this.state.ehrs) return null;
-		return <Table striped bordered hover>
-			<thead>
-			<tr>
-				<th>#</th>
-				<th>Patient ID</th>
-				<th>Gender</th>
-				<th>Sex</th>
-				<th>Vital Status</th>
-				<th>Birth Year</th>
-				<th>NHS Number</th>
-				<th>Time created</th>
-			</tr>
-			</thead>
-			<tbody>
-			{this.state.ehrs.map((e, index) => {
-				e.id = index + 1;
-				return PatientListEntry(e);
-			})}
-			</tbody>
-		</Table>;
-	}
+    render() {
+        if (!this.state.ehrs) return null;
+        return <Table striped bordered hover>
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Patient ID</th>
+                <th>Gender</th>
+                <th>Sex</th>
+                <th>Vital Status</th>
+                <th>Birth Year</th>
+                <th>NHS Number</th>
+                <th>Time created</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.ehrs.map((e, index) => {
+                e.id = index + 1;
+                return PatientListEntry(e);
+            })}
+            </tbody>
+        </Table>;
+    }
 }
 
 function PatientList() {
-	return (
-		<div>
-			<HeaderMenu/>
-			<Container>
-				<div style={{ height: '50px' }}></div>
-				<div style={{ display: 'block', textAlign: 'center' }}>
-					<div><h1>Patient List</h1></div>
-					<PatientListTable />
-				</div>
-			</Container>
+    return (
+        <div>
+            <HeaderMenu/>
+            <Container>
+                <div style={{ height: '50px' }}></div>
+                <div style={{ display: 'block', textAlign: 'center' }}>
+                    <div><h1>Patient List</h1></div>
+                    <PatientListTable/>
+                </div>
+            </Container>
 
-		</div>
-	);
+        </div>
+    );
 }
 
 export default PatientList;
