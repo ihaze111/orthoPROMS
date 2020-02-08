e07b6846b09c0bacf67e3e97774072ecab67cac8
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-02-08 18:09:08 +0000

    Brought getEhrId into utils

--- src/views/PatientUtils.js
+++ src/views/PatientUtils.js
@@ -1,5 +1,18 @@
 import qs from "qs";
-import getEHRId from "../components/GetEHRId";
+import CDROptions from "../components/CDROptions";
+const request = require('request-promise');
+
+async function getEHRId(subjectId) {
+    let processedResult;
+    const options = CDROptions.generateRequestOptions("/rest/v1/ehr/?subjectId=" + subjectId + "&subjectNamespace=uk.nhs.nhs_number");
+    await request(options, function (error, response) {
+            if (error) throw new Error(error);
+            const result = JSON.parse(response.body);
+            processedResult = result.ehrId;
+        }
+    );
+    return processedResult;
+}
 
 export function getSubjectId(locationSearch) {
     return qs.parse(locationSearch, { ignoreQueryPrefix: true }).subjectId ?
