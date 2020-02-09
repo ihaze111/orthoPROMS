2cc837fe8c4af3806c5b0045bfe29037ad842393
ihaze111 <shahrin.haziq@gmail.com>
2020-02-09 15:50:59 +0000

    I screwed up some stuff. Fixed just 1 graph. More to come!

---
+++ src/components/ScoresGraph.js
@@ -0,0 +1,60 @@
+import React from "react";
+import HighCharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const ScoresGraph = props => {
+    // console.log("Graph!");
+    // console.log(props.pain);
+    const painScore = props.pain;
+    const limitationsScore = props.limit;
+    const walkingScore = props.walking;
+    const walking_surfacesScore = props.surface;
+    const totalScore = props.total;
+    const regTime=props.time;
+
+    const options = {
+        chart: {
+            type : "line"
+        },
+        title: {
+            text : "Episode Scores"
+        },
+        xAxis: {
+            categories: regTime
+        },
+        series : [
+            {
+                name: "Pain Score",
+                data: painScore
+            },
+            {
+                name: "Limitations Score",
+                data: limitationsScore
+            },
+            {
+                name: "Walking Score",
+                data: walkingScore
+            },
+            {
+                name: "Walking Surfaces Score",
+                data: walking_surfacesScore
+            },
+            {
+                name: "Total Score",
+                data: totalScore
+            }
+        ]
+    };
+
+    return(
+        <div>
+            <HighchartsReact
+                highcharts={HighCharts}
+                constructorType={"chart"}
+                options={options}
+            />
+        </div>
+    );
+};
+
+export default ScoresGraph;
