7c026829b05918b596c5f679f4d40d3d12db6b06
ihaze111 <shahrin.haziq@gmail.com>
2020-02-15 17:55:45 +0000

    All graphs are live

---
+++ src/components/GetIndirectOximetry.js
@@ -0,0 +1,16 @@
+import CDRAQLQuery from "./CDRAQLQuery";
+
+async function getIndirectOximetry(ehrId) {
+    const aql = "select a_a/data[at0001]/events[at0002]/data[at0003]/items[at0006]/value/numerator as numerator, a_a/data[at0001]/events[at0002]/data[at0003]/items[at0006]/value/denominator as denominator, a_a/data[at0001]/origin/value as time from EHR e contains COMPOSITION a contains OBSERVATION a_a[openEHR-EHR-OBSERVATION.indirect_oximetry.v1] where e/ehr_id/value='" + ehrId + "'";
+    return await CDRAQLQuery(aql, (result) => {
+        if (result.resultSet) {
+            return result.resultSet.map((e) => {
+                return e;
+            });
+        } else {
+            return [];
+        }
+    });
+}
+
+export default getIndirectOximetry;
