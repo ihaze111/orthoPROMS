ed08b4f050367c6b616ac2a919b732e0c36bc5a8
ihaze111 <shahrin.haziq@gmail.com>
2020-02-21 19:31:02 +0000

    Added some minor stuff

--- src/components/Queries/GetAllergicList.js
+++ src/components/Queries/GetAllergicList.js
@@ -1,7 +1,7 @@
 import CDRAQLQuery from "./CDRAQLQuery";
 
 async function getAllergicList(ehrId) {
-    const aql = "select a_a/data[at0001]/items[at0025]/items[at0004]/value/value as reaction, a_a/data[at0001]/items[at0025]/items[at0022]/value/value as comment, a_a/data[at0001]/items[at0002]/value/value as cause, a_b/data[at0001]/items[at0002.1]/value/value as exclusion, a_b/protocol[at0006]/items[at0004]/value/value as update_exclusion_date from EHR e contains COMPOSITION a contains ( EVALUATION a_a[openEHR-EHR-EVALUATION.adverse_reaction_uk.v1] or EVALUATION a_b[openEHR-EHR-EVALUATION.exclusion-adverse_reaction.v1]) where e/ehr_id/value='" + ehrId + "'";
+    const aql = "select a_a/data[at0001]/items[at0025]/items[at0004]/value/value as reaction, a_a/data[at0001]/items[at0025]/items[at0022]/value/value as comment, a_a/data[at0001]/items[at0002]/value/value as cause, a_b/data[at0001]/items[at0002.1]/value/value as exclusion, a_b/protocol[at0006]/items[at0004]/value/value as update_exclusion_date, a/composer/name as composer from EHR e contains COMPOSITION a contains ( EVALUATION a_a[openEHR-EHR-EVALUATION.adverse_reaction_uk.v1] or EVALUATION a_b[openEHR-EHR-EVALUATION.exclusion-adverse_reaction.v1]) where e/ehr_id/value='" + ehrId + "'";
     return await CDRAQLQuery(aql, (result) => {
         if (result.resultSet) {
             return result.resultSet.map((e) => {
