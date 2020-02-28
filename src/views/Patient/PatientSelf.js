8acf4d3b16400bb456061f92975ee879b65a28a3
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-02-28 23:19:48 +0000

    Moved template to new submodule system

--- src/views/Patient/PatientSelf.js
+++ src/views/Patient/PatientSelf.js
@@ -34,35 +34,6 @@ class PatientSelf extends React.Component {
     componentDidMount() {
         loadEhrId.call(this);
 
-        // if (this.props.location.search !== "") {
-        //     const compositionSring = {
-        //         "ctx/language": "en",
-        //         "ctx/territory": "GB",
-        //         "ctx/composer_name": "Silvia Blake",
-        //         "ctx/id_namespace": "HOSPITAL-NS",
-        //         "ctx/id_scheme": "HOSPITAL-NS",
-        //         "ctx/health_care_facility|name": "Hospital",
-        //         "ctx/health_care_facility|id": "9091",
-        //         "uclh_foot_and_ankle_proms/aofas_score/q1_pain|code": "at0032"
-        //     };
-        //     // compositionSring.templateId = "Foot_and_Ankle_PROMs-v0";
-        //     // compositionSring.ehrId = "d9668d3d-85fa-488f-97f3-53c8765c22fb";
-        //     const getVariables = qs.parse(this.props.location.search, { ignoreQueryPrefix: true });
-        //     console.log(getVariables);
-        //     for (let x in getVariables) {
-        //         compositionSring["uclh_foot_and_ankle_proms/aofas_score/" + x + "|code"] = getVariables[x];
-        //     }
-        //     var request = require('request');
-        //     var options = {
-        //         'method': 'POST',
-        //         'url':
-        // 'https://cdr.code4health.org/rest/v1/composition?ehrId=b80a3a97-be75-41c6-a497-6ed53ce8f8c6&templateId=Foot_and_Ankle_PROMs-v0&committerName=Dr
-        // nullnull&format=FLAT', 'headers': { 'Ehr-Session-disabled': '{{Ehr-Session}}', 'Content-Type':
-        // 'application/json', 'Authorization': 'Basic
-        // YmIyNjRiY2UtYzQwNy00OTgyLTkwMTctOTdkMzcyN2ZjZmE0OiQyYSQxMCQ2MTlraQ==' }, body:
-        // JSON.stringify(compositionSring) }; request(options, function (error, response) { if (error) throw new
-        // Error(error); console.log(response.body); }); }
-
         $('#submitSurveyDialog').hide();
 
     }
--- src/views/Patient/Template.js
+++ src/views/Patient/Template.js
@@ -1,6 +1,43 @@
 import React from 'react';
+import CDROptions from "../../components/CDROptions";
+
 import { Form, Input, RadioGroup } from 'formsy-react-components';
-import getTemplate from "../../components/GetTemplate";
+import getWebTemplate from "../../components/GetWebTemplate";
+import JsonFormInputToReact from "../../ehr-template-react-generator/view";
+
+function commitComposition(model) {
+    const compositionSring = {
+        "ctx/language": "en",
+        "ctx/territory": "GB",
+        "ctx/composer_name": "Silvia Blake",
+        "ctx/id_namespace": "HOSPITAL-NS",
+        "ctx/id_scheme": "HOSPITAL-NS",
+        "ctx/health_care_facility|name": "Hospital",
+        "ctx/health_care_facility|id": "9091",
+    };
+    compositionSring.templateId = "Foot_and_Ankle_PROMs-v0";
+    compositionSring.ehrId = "d9668d3d-85fa-488f-97f3-53c8765c22fb";
+    for (let x in model) {
+        compositionSring["uclh_foot_and_ankle_proms/aofas_score/" + x + "|code"] = model[x];
+    }
+    var request = require('request');
+    var options = {
+        'method': 'POST',
+        'url':
+            'https://cdr.code4health.org/rest/v1/composition?ehrId=b80a3a97-be75-41c6-a497-6ed53ce8f8c6&templateId=Foot_and_Ankle_PROMs-v0&committerName=Drnullnull&format=FLAT',
+        'headers': {
+            'Ehr-Session-disabled': '{{Ehr-Session}}', 'Content-Type':
+                'application/json', 'Authorization': CDROptions.CDRHeaders.Authorization
+        }, body:
+            JSON
+                .stringify(compositionSring)
+    };
+    request(options, function (error, response) {
+        if (error) throw new
+        Error(error);
+        console.log(response.body);
+    });
+}
 
 export default class Template extends React.Component {
     constructor(props) {
@@ -11,7 +48,7 @@ export default class Template extends React.Component {
     }
 
     componentDidMount() {
-        let promise = getTemplate();
+        let promise = getWebTemplate();
         promise.then((e) => {
             this.setState({ canSubmit: this.state.canSubmit, template: e });
         });
@@ -26,36 +63,22 @@ export default class Template extends React.Component {
     }
 
     submit(model) {
-        console.log(JSON.stringify(model));
+        commitComposition(model);
     }
 
     render() {
         if (!this.state.template) return null;
+        const sample = this.state.template;
         return (
-            <Form onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
-                {this.state.template.map((e) => {
-                        if (e.inputType === 'radio') {
-                            const radioOptions = e.inputs[0].list.map((input) => ({
-                                value: input.value,
-                                label: input.label
-                            }));
-                            return <RadioGroup
-                                name={e.id}
-                                key={e.id}
-                                label={e.name}
-                                help={e.description}
-                                options={radioOptions}
-                                required
-                            />
-                        } else {
-                            return <Input name={e.id} key={e.id} label={e.name} help={e.description}/>
-                        }
-                    }
-                )}
-
-                <input style={{ marginLeft: "50%" }} className="btn btn-primary" type="submit"
-                       disabled={!this.state.canSubmit} defaultValue="Submit"/>
-            </Form>
+            <div>
+                <Form onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+                    {sample.map((jsonInputObject) => {
+                        return JsonFormInputToReact(jsonInputObject);
+                    })}
+                    <input style={{ marginLeft: "50%" }} className="btn btn-primary" type="submit"
+                           disabled={!this.state.canSubmit} defaultValue="Submit"/>
+                </Form>
+            </div>
         );
     }
 }
