cbb620cb202a76486c90de5f609b8a9a0d5687b5
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-06 12:55:13 +0000

    Started using hooks

--- src/views/Patient/PatientSelf.js
+++ src/views/Patient/PatientSelf.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Tab from 'react-bootstrap/Tab';
@@ -9,7 +9,7 @@ import $ from 'jquery';
 import HeaderMenu from "../../components/HeaderMenu";
 import * as PropTypes from "prop-types";
 import { PatientOverview, PatientProgressTable, ScoresArray, EpisodeScoresGraph } from "../PatientComponents";
-import { getSubjectId, loadEhrId } from "../PatientUtils";
+import { getSubjectId, getEHRId } from "../PatientUtils";
 import Template from "./Template";
 
 function SurveySuccess() {
@@ -23,73 +23,61 @@ function SurveySuccess() {
 
 SurveySuccess.propTypes = { onClose: PropTypes.func };
 
-class PatientSelf extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            painArray: []
-        };
-    }
-
-    componentDidMount() {
-        loadEhrId.call(this);
-
-        $('#submitSurveyDialog').hide();
-
-    }
-
-    render() {
-        let subjectId = getSubjectId(this.props.location.search);
-        return (
-            <div>
-                <HeaderMenu/>
-                <Container style={{ marginTop: '50px' }}>
-                    <PatientOverview subjectId={subjectId}/>
-                    <Card>
-                        <Card.Header>
-                            <Card.Title>Details</Card.Title>
-                        </Card.Header>
-                        <Card.Body>
-                            <Tab.Container defaultActiveKey="myProgress">
-                                <Nav variant="tabs" style={{ marginBottom: '40px' }}>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="myProgress">My Progress</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="myData">My Data</Nav.Link>
-                                    </Nav.Item>
-                                    <Nav.Item>
-                                        <Nav.Link eventKey="survey">Survey</Nav.Link>
-                                    </Nav.Item>
-                                    {/* <Nav.Item>
+function PatientSelf(props) {
+    let subjectId = getSubjectId(props.location.search);
+    const [ehrId1, setEhrId1] = useState('');
+    getEHRId(subjectId).then((e) => {
+        setEhrId1(e);
+    });
+    return (
+        <div>
+            <HeaderMenu/>
+            <Container style={{ marginTop: '50px' }}>
+                <PatientOverview subjectId={subjectId}/>
+                <Card>
+                    <Card.Header>
+                        <Card.Title>Details</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                        <Tab.Container defaultActiveKey="myProgress">
+                            <Nav variant="tabs" style={{ marginBottom: '40px' }}>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="myProgress">My Progress</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="myData">My Data</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="survey">Survey</Nav.Link>
+                                </Nav.Item>
+                                {/* <Nav.Item>
                                         <Nav.Link eventKey="test">Test</Nav.Link>
                                     </Nav.Item> */}
-                                </Nav>
-                                <Tab.Content>
-                                    <Tab.Pane eventKey="myProgress">
-                                        <PatientProgressTable ehrId={this.state.ehrId}/>
-                                    </Tab.Pane>
-                                    {/* <Tab.Pane eventKey="test">
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="myProgress">
+                                    <PatientProgressTable ehrId={ehrId1}/>
+                                </Tab.Pane>
+                                {/* <Tab.Pane eventKey="test">
                                         <div style={{width: '1000px', height: '400px'}}><EpisodeScoresGraph ehrId={this.state.ehrId}/></div>
                                     </Tab.Pane> */}
-                                    <Tab.Pane eventKey="myData">
-                                        <div><ScoresArray ehrId={this.state.ehrId}/></div>
-                                        <br/><br/><br/>
-                                        <div style={{ width: '1000px', height: '400px' }}><EpisodeScoresGraph
-                                            ehrId={this.state.ehrId}/></div>
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="survey">
-                                        <Template/>
-                                        <SurveySuccess/>
-                                    </Tab.Pane>
-                                </Tab.Content>
-                            </Tab.Container>
-                        </Card.Body>
-                    </Card>
-                </Container>
-            </div>
-        );
-    }
+                                <Tab.Pane eventKey="myData">
+                                    <div><ScoresArray ehrId={ehrId1}/></div>
+                                    <br/><br/><br/>
+                                    <div style={{ width: '1000px', height: '400px' }}><EpisodeScoresGraph
+                                        ehrId={ehrId1}/></div>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="survey">
+                                    <Template/>
+                                    <SurveySuccess/>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Card.Body>
+                </Card>
+            </Container>
+        </div>
+    );
 }
 
 export default PatientSelf;
