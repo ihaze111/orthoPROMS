7935d87b37444f896ecad560beb8bdf5af0ede66
ihaze111 <shahrin.haziq@gmail.com>
2020-03-06 17:29:17 +0000

    Fix the time created in patient list

--- src/components/Queries/GetEHRs.js
+++ src/components/Queries/GetEHRs.js
@@ -1,20 +1,23 @@
 import CDRAQLQuery from "./CDRAQLQuery";
 
 function callbackProcessing(result) {
+    
     return result.resultSet.map((e) => {
+        console.log(result.resultSet);
         let final = {};
         final.patientId = e.ehr_id.value || "";
-        if (e.other_details) {
-            final.gender = e.other_details.items[0].items[0].value.value || "";
-            final.sex = e.other_details.items[0].items[1].value.value || "";
-            final.vitalStatus = e.other_details.items[0].items[2].value.value || "";
-            final.birthYear = e.other_details.items[0].items[3].value.value || "";
-            final.nhsNumber = e.nhsNumber.value || "";
-            final.timeCreated = e.time_created.value || "";
-        }
-        if(e.nhsNumber) {
-            final.subjectId = e.nhsNumber.value;
-        }
+        // if (e.other_details) {
+            final.gender = e.other_details ? e.other_details.items[0].items[0].value.value : "";
+            final.sex = e.other_details ? e.other_details.items[0].items[1].value.value : "";
+            // final.vitalStatus = e.other_details ? e.other_details.items[0].items[2].value.value : "";
+            // final.birthYear = e.other_details ? e.other_details.items[0].items[3].value.value : "";
+            final.nhsNumber = e.nhsNumber ? e.nhsNumber.value : "";
+            final.timeCreated = e.time_created ? e.time_created.value.replace(/T/, ",").substring(0, e.time_created.value.indexOf('.')) : "";
+        // }
+        // if(e.nhsNumber) {
+            final.subjectId = e.nhsNumber ? e.nhsNumber.value : "";
+        // }
+        console.log(final);
         return final;
     });
 }
