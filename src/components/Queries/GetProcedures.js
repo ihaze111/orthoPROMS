1a95a4400b45f6cb4ddbfcb74d1b602275c07651
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-07 16:10:57 +0000

    Simplified logic where we were mapping over but not changing anything

--- src/components/Queries/GetProcedures.js
+++ src/components/Queries/GetProcedures.js
@@ -3,13 +3,7 @@ import CDRAQLQuery from "./CDRAQLQuery";
 async function getProcedures(ehrId) {
     const aql = "select a_a/description[at0001]/items[at0002]/value/value as procedure_name, a_a/description[at0001]/items[at0049]/value/value as notes, a_a/time/value as time, a/composer/name as name, a_a/ism_transition/careflow_step/value as careflow_step, a_a/ism_transition/current_state/value as current_state from EHR e contains COMPOSITION a contains ACTION a_a[openEHR-EHR-ACTION.procedure.v1] where e/ehr_id/value='" + ehrId + "'";
     return await CDRAQLQuery(aql, (result) => {
-        if (result.resultSet) {
-            return result.resultSet.map((e) => {
-                return e;
-            });
-        } else {
-            return [];
-        }
+        return result.resultSet ? result.resultSet : [];
     });
 }
 
