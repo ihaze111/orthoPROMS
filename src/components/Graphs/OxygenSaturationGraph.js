7de6fb7272b92b6ea66e81f83145db23cd4f0c49
ihaze111 <shahrin.haziq@gmail.com>
2020-03-09 03:17:03 +0000

    Highcharts Line Graph To Chart.js

--- src/components/Graphs/OxygenSaturationGraph.js
+++ src/components/Graphs/OxygenSaturationGraph.js
@@ -1,60 +1,168 @@
-import React from "react";
-import HighCharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
+// import React from "react";
+// import HighCharts from "highcharts";
+// import HighchartsReact from "highcharts-react-official";
 
-const OxygenSaturationGraph = props => {
-    const percentage = props.percent;
-    const time = props.time;
+// const OxygenSaturationGraph = props => {
+//     const percentage = props.percent;
+//     const time = props.time;
 
-    const options = {
-        chart: {
-            type : "line"
-        },
-        title: {
-            text : "Oxygen Concentration (Indirect Oximetry)"
-        },
-        xAxis: {
-            title : {
-                text : "Time"
+//     const options = {
+//         chart: {
+//             type : "line"
+//         },
+//         title: {
+//             text : "Oxygen Concentration (Indirect Oximetry)"
+//         },
+//         xAxis: {
+//             title : {
+//                 text : "Time"
+//             },
+//             categories: time
+//         },
+//         yAxis: {
+//             min: 0,
+//             title : {
+//                 text : "Oxygen Concentration / %"
+//             },
+//             labels: {
+//                 overflow: 'justify'
+//             }
+//         },
+//         plotOptions: {
+//             bar: {
+//                 dataLabels: {
+//                     enabled: true
+//                 }
+//             }
+//         },
+//         tooltip:{
+//             valueSuffix : "%"
+//         },
+//         series : [
+//             {
+//                 name: "Oxygen Concentration",
+//                 data: percentage
+//             }
+//         ]
+//     };
+
+//     return(
+//         <div>
+//             <HighchartsReact
+//                 highcharts={HighCharts}
+//                 constructorType={"chart"}
+//                 options={options}
+//             />
+//         </div>
+//     );
+// };
+
+import React, { Component } from 'react'
+import Chart from "chart.js";
+let myOxygenSaturationGraph;
+
+Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif"
+Chart.defaults.global.legend.display = true;
+
+class OxygenSaturationGraph extends Component {
+    chartRef = React.createRef();
+
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+        this.buildChart();
+    }
+
+    componentDidUpdate() {
+        this.buildChart();
+    }
+
+    buildChart = () => {
+        const myOxygenSaturationGraphRef = this.chartRef.current.getContext("2d");
+
+        if (typeof myOxygenSaturationGraph !== "undefined") myOxygenSaturationGraph.destroy();
+
+        myOxygenSaturationGraph= new Chart(myOxygenSaturationGraphRef, {
+            type: "line",
+            data: {
+                labels: this.props.time,
+                datasets: [
+                    {
+                        label: "Oxygen Concentration",
+                        data: this.props.percent,
+                        fill: false,
+                        borderColor: "blue"
+                    }
+                ]
             },
-            categories: time
-        },
-        yAxis: {
-            min: 0,
-            title : {
-                text : "Oxygen Concentration / %"
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
+            options: {
+                title : {
+                    display: true,
+                    text : "Oxygen Concentration (Indirect Oximetry)",
+                    fontSize: 18,
+                    fontFamily: 'Lucida',
+                    fontColor: '#000'
+                    
+                },
+                legend: {
+                    display:true,
+                    position: 'bottom',
+                    labels: {
+                        fontColor: '#000'
+                    }
+                },
+                tooltips: {
+                    enabled: true,
+                    displayColors: true,
+                    mode: 'single',
+                    callbacks: {
+                        label: function (tooltipItems, data){
+                            return tooltipItems.yLabel + "%";
+                        }
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            display : true,
+                            drawBorder: true,
+                            drawOnChartArea : false
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Date/Time"
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            display : true,
+                            drawBorder: true,
+                            drawOnChartArea : true
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Oxygen Concentration / %"
+                        }
+                    }]
                 }
             }
-        },
-        tooltip:{
-            valueSuffix : "%"
-        },
-        series : [
-            {
-                name: "Oxygen Concentration",
-                data: percentage
-            }
-        ]
-    };
+        });
 
-    return(
-        <div>
-            <HighchartsReact
-                highcharts={HighCharts}
-                constructorType={"chart"}
-                options={options}
-            />
-        </div>
-    );
-};
+    }
+
+    render() {
+
+        return (
+            <div className="oxygen">
+                <canvas
+                    id="myScores"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
 
 export default OxygenSaturationGraph;
--- src/components/Graphs/RespirationRateGraph.js
+++ src/components/Graphs/RespirationRateGraph.js
@@ -1,50 +1,158 @@
-import React from "react";
-import HighCharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
+// import React from "react";
+// import HighCharts from "highcharts";
+// import HighchartsReact from "highcharts-react-official";
 
-const RespirationRateGraph = props => {
-    const respirationRateMagnitude = props.magnitude;
-    const time = props.time;
-    const unit = props.units;
+// const RespirationRateGraph = props => {
+//     const respirationRateMagnitude = props.magnitude;
+//     const time = props.time;
+//     const unit = props.units;
 
-    const options = {
-        chart: {
-            type : "line"
-        },
-        title: {
-            text : "Respiration Rate"
-        },
-        xAxis: {
-            title : {
-                text : "Time"
+//     const options = {
+//         chart: {
+//             type : "line"
+//         },
+//         title: {
+//             text : "Respiration Rate"
+//         },
+//         xAxis: {
+//             title : {
+//                 text : "Time"
+//             },
+//             categories: time
+//         },
+//         yAxis: {
+//             title : {
+//                 text : "Rate" + unit
+//             }
+//         },
+//         tooltip:{
+//             valueSuffix : unit
+//         },
+//         series : [
+//             {
+//                 name: "Respiration Rate",
+//                 data: respirationRateMagnitude
+//             }
+//         ]
+//     };
+
+//     return(
+//         <div>
+//             <HighchartsReact
+//                 highcharts={HighCharts}
+//                 constructorType={"chart"}
+//                 options={options}
+//             />
+//         </div>
+//     );
+// };
+
+import React, { Component } from 'react'
+import Chart from "chart.js";
+let myRespirationRateGraph;
+
+Chart.defaults.global.defaultFontFamily = "'PT Sans', sans-serif"
+Chart.defaults.global.legend.display = true;
+
+class RespirationRateGraph extends Component {
+    chartRef = React.createRef();
+
+    constructor(props){
+        super(props);
+    }
+
+    componentDidMount() {
+        this.buildChart();
+    }
+
+    componentDidUpdate() {
+        this.buildChart();
+    }
+
+    buildChart = () => {
+        const myRespirationRateGraphRef = this.chartRef.current.getContext("2d");
+
+        if (typeof myRespirationRateGraph !== "undefined") myRespirationRateGraph.destroy();
+        const unit = this.props.units;
+        myRespirationRateGraph= new Chart(myRespirationRateGraphRef, {
+            type: "line",
+            data: {
+                labels: this.props.time,
+                datasets: [
+                    {
+                        label: "Respiration Rate",
+                        data: this.props.magnitude,
+                        fill: false,
+                        borderColor: "blue"
+                    }
+                ]
             },
-            categories: time
-        },
-        yAxis: {
-            title : {
-                text : "Rate" + unit
+            options: {
+                title : {
+                    display: true,
+                    text : "Respiration Rate",
+                    fontSize: 18,
+                    fontFamily: 'Lucida',
+                    fontColor: '#000'
+                    
+                },
+                legend: {
+                    display:true,
+                    position: 'bottom',
+                    labels: {
+                        fontColor: '#000'
+                    }
+                },
+                tooltips: {
+                    enabled: true,
+                    displayColors: true,
+                    mode: 'single',
+                    callbacks: {
+                        label: function (tooltipItems, data){
+                            return tooltipItems.yLabel + unit;
+                        }
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            display : true,
+                            drawBorder: true,
+                            drawOnChartArea : false
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Date/Time"
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            display : true,
+                            drawBorder: true,
+                            drawOnChartArea : true
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: "Rate " + unit
+                        }
+                    }]
+                }
             }
-        },
-        tooltip:{
-            valueSuffix : unit
-        },
-        series : [
-            {
-                name: "Respiration Rate",
-                data: respirationRateMagnitude
-            }
-        ]
-    };
+        });
 
-    return(
-        <div>
-            <HighchartsReact
-                highcharts={HighCharts}
-                constructorType={"chart"}
-                options={options}
-            />
-        </div>
-    );
-};
+    }
+
+    render() {
+
+        return (
+            <div className="respiration">
+                <canvas
+                    id="myRespirationRate"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
 
 export default RespirationRateGraph;
