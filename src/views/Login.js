d65e533297fd235fd35d0d69de0568adcac89a90
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-10 11:40:26 +0000

    Made Login buttons NHS

--- src/views/Login.js
+++ src/views/Login.js
@@ -4,6 +4,7 @@ import $ from  'jquery';
 import HeaderMenu from "../components/HeaderMenu";
 import qs from "qs";
 import Button from 'react-bootstrap/Button';
+import { NHSButton, NHSFormControl, NHSFormGroup, NHSFormLabel } from './NHSComponents';
 
 class Login extends React.Component{
     handleClick() {
@@ -28,21 +29,19 @@ class Login extends React.Component{
                 <HeaderMenu />
                 <div style={{display: 'flex', alignSelf:'center', justifyContent: 'center', marginTop: '10%'}}>
                     <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                    </Form.Group>
+                    <NHSFormGroup controlId="formBasicEmail">
+                        <NHSFormLabel>Email address</NHSFormLabel>
+                        <NHSFormControl type="email" placeholder="Enter email" />
+                    </NHSFormGroup>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                    <NHSFormGroup controlId="formBasicPassword">
+                        <NHSFormLabel>Password</NHSFormLabel>
+                        <NHSFormControl type="password" placeholder="Password" />
+                    </NHSFormGroup>
                     <Form.Group controlId="formBasicCheckbox">
                         <Form.Check type="checkbox" label="Keep me logged in" />
                     </Form.Group>
-                    <Button className="btn btn-primary btn-block login">Log in</Button>
+                    <NHSButton className="login">Log in</NHSButton>
                     <a href="/Register">Register</a><br />
                     <Button variant="link" onClick={this.handleClick}>Forgot your password?</Button>
                     </Form>
