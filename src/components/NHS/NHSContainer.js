24da33916c574c1dce5f0abbd85b6f3154ef4e39
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-11 00:03:15 +0000

    Refactored NHS container and wrapper

--- src/components/NHS/NHSContainer.js
+++ src/components/NHS/NHSContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NHSContainer(props) {
-    return <div className="nhsuk-width-container">{props.children}</div>;
+    return <div className="nhsuk-width-container" {...props}/>;
 }
 
 export default NHSContainer;
