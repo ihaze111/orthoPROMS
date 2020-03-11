546806e1cbed9263ccdfb55d9e3e5f156e23238b
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-11 00:08:26 +0000

    Changed back link to NHS

--- src/components/NHS/NHSIcons.js
+++ src/components/NHS/NHSIcons.js
@@ -23,3 +23,11 @@ export function NHSVectorSearch() {
             d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
     </svg>;
 }
+
+export function NHSVectorChevronLeft() {
+    return <svg className="nhsuk-icon nhsuk-icon__chevron-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                aria-hidden="true">
+        <path
+            d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"></path>
+    </svg>;
+}
