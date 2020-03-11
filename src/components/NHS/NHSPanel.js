3131a5b632cd5232800f8a64710e3170e080c3dc
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-11 00:18:53 +0000

    Moved to NHS Panels for about page

---
+++ src/components/NHS/NHSPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function NHSPanelTitle(props) {
+    return <h3 {...props} />
+}
+
+export function NHSPanelBody(props) {
+    return <div {...props} />;
+}
+export function NHSPanel(props) {
+    return <div className="nhsuk-panel" {...props} />;
+}
