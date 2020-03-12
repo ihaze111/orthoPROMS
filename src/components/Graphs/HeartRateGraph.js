33afb8895406baf9ca04def0e8a4426dab3bd5c9
ihaze111 <shahrin.haziq@gmail.com>
2020-03-12 19:31:10 +0000

    Some graphs

--- src/components/Graphs/HeartRateGraph.js
+++ src/components/Graphs/HeartRateGraph.js
@@ -1,61 +1,78 @@
-import React from "react";
-import HighCharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
+import React,{ Component } from "react";
+import GeneralLineChart from "./GeneralLineChart";
+// import HighCharts from "highcharts";
+// import HighchartsReact from "highcharts-react-official";
 
-const HeartRateGraph = props => {
-    const heartRate = props.heartRates;
-    const time = props.time;
-    const unit = props.units
+class HeartRateGraph extends Component{
+    render(){
+        return (
+            <GeneralLineChart id={"myHeart"} labels={this.props.time} data={
+                [
+                    {
+                        label : "Heart Rate",
+                        data: this.props.heartRates
+                    }
+                ]
+            }
+            title={"Heart Rate"} xLabel={"Date/Time"} yLabel={"Heart Rate " + this.props.units}/>
+        )
+    }
+}
 
-    const options = {
-        chart: {
-            type : "area"
-        },
-        title: {
-            text : "Heart Rate"
-        },
-        xAxis: {
-            title : {
-                text : "Time"
-            },
-            categories: time
-        },
-        yAxis: {
-            min: 0,
-            title : {
-                text : "Heart Rate " + unit
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        tooltip:{
-            valueSuffix : unit
-        },
-        series : [
-            {
-                name: "Heart Rate",
-                data: heartRate
-            }
-        ]
-    };
+// const HeartRateGraph = props => {
+//     const heartRate = props.heartRates;
+//     const time = props.time;
+//     const unit = props.units
 
-    return(
-        <div>
-            <HighchartsReact
-                highcharts={HighCharts}
-                constructorType={"chart"}
-                options={options}
-            />
-        </div>
-    );
-};
+//     const options = {
+//         chart: {
+//             type : "area"
+//         },
+//         title: {
+//             text : "Heart Rate"
+//         },
+//         xAxis: {
+//             title : {
+//                 text : "Time"
+//             },
+//             categories: time
+//         },
+//         yAxis: {
+//             min: 0,
+//             title : {
+//                 text : "Heart Rate " + unit
+//             },
+//             labels: {
+//                 overflow: 'justify'
+//             }
+//         },
+//         plotOptions: {
+//             bar: {
+//                 dataLabels: {
+//                     enabled: true
+//                 }
+//             }
+//         },
+//         tooltip:{
+//             valueSuffix : unit
+//         },
+//         series : [
+//             {
+//                 name: "Heart Rate",
+//                 data: heartRate
+//             }
+//         ]
+//     };
+
+//     return(
+//         <div>
+//             <HighchartsReact
+//                 highcharts={HighCharts}
+//                 constructorType={"chart"}
+//                 options={options}
+//             />
+//         </div>
+//     );
+// };
 
 export default HeartRateGraph;
