7f9b96e022f187c07b6f66bac8bf7d1684e7112f
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-12 00:52:50 +0000

    Fixed buttons on homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component{
 	render(){
 		return(
 			<div>
-				
+
 				<div style={{backgroundImage: 'url(./1.jpg)', height: '100vh', backgroundPosition: 'center center', backgroundSize: 'cover', textAlign: 'center !important'}}>
 					<div style={{justifyContent: 'center', paddingTop: '18%'}}>
 						<div style={{paddingLeft: '30%'}}>
@@ -21,15 +21,13 @@ class Home extends React.Component{
 						{/* <ButtonToolbar style={{marginLeft: '28%'}}> */}
 						<div style={{paddingLeft: '40%', display:'inline-block', paddingTop: '5%'}}>
 						<a href="/Login?id=1" style={{ color: "white"}}>
-						<NHSButton className="patientButton">
+						<NHSButton>
 						Patient
-						{/* <a href="/Login?id=1" className="btn btn-primary btn-lg" style={{fontSize: '2.25rem'}}>Patient</a> */}
 						</NHSButton></a></div>
 						<div style={{display:'inline-block', paddingLeft: '15%'}}>
 						<a href="/Login?id=2" style={{ color: "white"}}>
-						<NHSButtonSecondary className="clinicianButton">
+						<NHSButtonSecondary>
 						Clinicians
-						{/* <a href="/Login?id=2" className="btn btn-success btn-lg" style={{fontSize: '2.25rem'}}>Clinicians</a> */}
 						</NHSButtonSecondary></a>
 						</div>
 						{/* </ButtonToolbar> */}
