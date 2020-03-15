186633ed5174bea06d9e99b33bf906481ea238ba
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-15 13:06:25 +0000

    Moved template to structured

--- src/components/GetWebTemplate.js
+++ src/components/GetWebTemplate.js
@@ -1,22 +1,27 @@
 import CDROptions from "./Queries/CDROptions";
-import { inputToJsonFormInput, treeTrawlGettingFlatInputs } from "../ehr-template-processor/src/template";
+import {
+    inputToJsonFormInput,
+    treeTrawlGettingFlatInputs,
+    treeTrawlGettingStructuredInputs
+} from "../ehr-template-processor/src/template";
 
 const request = require('request-promise');
 
-async function getWebTemplate() {
+async function getWebTemplate(templateName) {
     let processedResult = [];
-    const options = CDROptions.generateRequestOptions("/rest/v1/template/Foot_and_Ankle_PROMs-v0");
+    const options = CDROptions.generateRequestOptions("/rest/v1/template/" + templateName);
     await request(options, function (error, response) {
             if (error) throw new Error(error);
             const result = JSON.parse(response.body);
             let template = result.webTemplate;
             let language = template.defaultLanguage;
             let tree = template.tree;
-            const result1 = [];
-            treeTrawlGettingFlatInputs(tree, language, [], []).map((input) => {
-                result1.push(inputToJsonFormInput(input, language));
-            });
-            processedResult = result1;
+            // const result1 = [];
+            // treeTrawlGettingFlatInputs(tree, language, [], []).map((input) => {
+            //     result1.push(inputToJsonFormInput(input, language));
+            // });
+            // processedResult = result1;
+            processedResult = treeTrawlGettingStructuredInputs(tree, language, []);
         }
     );
     return processedResult;
