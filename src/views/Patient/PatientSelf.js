1231ecf2ef062b400934cd83355a83a8711ec1ac
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-17 19:15:12 +0000

    changed to foot and ankle

--- src/views/Patient/PatientSelf.js
+++ src/views/Patient/PatientSelf.js
@@ -77,8 +77,8 @@ function PatientSelf(props) {
                                     <div><Chart1 bingbong="bingbong"/></div> */}
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="survey">
-                                        {/*<StructuredSurvey templateId={'Foot_and_Ankle_PROMs-v0'}/>*/}
-                                        <StructuredSurvey templateId={'WHO - Suspected Covid-19 assessment.v0'}/>
+                                        <StructuredSurvey templateId={'Foot_and_Ankle_PROMs-v0'}/>
+                                        {/*<StructuredSurvey templateId={'WHO - Suspected Covid-19 assessment.v0'}/>*/}
                                         {/*<FlatSurvey templateId={'Foot_and_Ankle_PROMs-v0'}/>*/}
                                         <SurveySuccess/>
                                     </Tab.Pane>
