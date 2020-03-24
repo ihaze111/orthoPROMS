f32eb7aaac302de9b8bcb7037e98f8e622e755af
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-24 10:08:40 +0000

    Formatting changes

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -2,23 +2,23 @@ import { SET_CURRENT_USER } from '../constants'
 import isEmpty from 'lodash/isEmpty' // lodash工具库 使用里面判断是否为空的方法
 
 const initialState = {
-  isAuthenticated: false, // 判断是否登录
-  user: {}, // 登录后获取用户信息
-  isGoogleLogin: false // 判断是否是第三方登录
+    isAuthenticated: false, // 判断是否登录
+    user: {}, // 登录后获取用户信息
+    isGoogleLogin: false // 判断是否是第三方登录
 }
 
-
 const auth = (state = initialState, action = {}) => {
-  switch(action.type){
-    case SET_CURRENT_USER:
-      console.log(action)
-      return {
-        isAuthenticated: !isEmpty(action.user), 
-        user: action.user,
-        isGoogleLogin: action.isGoogleLogin
-      }
-    default: return state;
-  }
+    switch (action.type) {
+        case SET_CURRENT_USER:
+            console.log(action)
+            return {
+                isAuthenticated: !isEmpty(action.user),
+                user: action.user,
+                isGoogleLogin: action.isGoogleLogin
+            }
+        default:
+            return state;
+    }
 }
 
 export default auth;
--- src/reducers/clinician.js
+++ src/reducers/clinician.js
@@ -1,42 +1,41 @@
 import * as _ from 'lodash'
 
-
 const initialState = {
-  patientList: [],
-  patientListFiltered: [],
-  search: ''
+    patientList: [],
+    patientListFiltered: [],
+    search: ''
 }
 
-
-function search(state,data){
-  let patientList = state.patientList,
-      patientListFiltered = []
-  let keys = ['nhsNumber','gender','sex','vitalStatus','birthYear']
-  let result = []
-  if( data ){
-      keys.forEach(key => {
-        let tmp  =  patientList.filter(e =>{
-          return e[key] && e[key].includes(data)
+function search(state, data) {
+    let patientList = state.patientList,
+        patientListFiltered = []
+    let keys = ['nhsNumber', 'gender', 'sex', 'vitalStatus', 'birthYear']
+    let result = []
+    if (data) {
+        keys.forEach(key => {
+            let tmp = patientList.filter(e => {
+                return e[key] && e[key].includes(data)
+            })
+            if (tmp.length > 0) {
+                result = [...result, ...tmp]
+            }
         })
-        if( tmp.length > 0 ) {
-          result = [...result,...tmp]
-      }
-    })
-    patientListFiltered = _.uniqWith(result, _.isEqual)
-    return Object.assign({},state,{patientListFiltered,search: data})
-  }
-  return Object.assign({},state,{search: data,patientListFiltered: patientList})
+        patientListFiltered = _.uniqWith(result, _.isEqual)
+        return Object.assign({}, state, { patientListFiltered, search: data })
+    }
+    return Object.assign({}, state, { search: data, patientListFiltered: patientList })
 }
 
 
 const clinician = (state = initialState, action = {}) => {
-  switch( action.type ){
-    case 'SET_PATIENTLIST':
-      return Object.assign({},state,{patientList: action.data,patientListFiltered: action.data});
-    case 'handleCliniSearch': 
-      return search(state, action.data);
-    default: return state;
-  }
+    switch (action.type) {
+        case 'SET_PATIENTLIST':
+            return Object.assign({}, state, { patientList: action.data, patientListFiltered: action.data });
+        case 'handleCliniSearch':
+            return search(state, action.data);
+        default:
+            return state;
+    }
 }
 
 export default clinician;
