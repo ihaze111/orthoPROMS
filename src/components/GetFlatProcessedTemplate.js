4aaee4279418a4d1f255f009f6d65135cf51d197
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-26 19:24:37 +0000

    Fixed imports

--- src/components/GetFlatProcessedTemplate.js
+++ src/components/GetFlatProcessedTemplate.js
@@ -1,7 +1,5 @@
 import CDROptions from "./Queries/CDROptions";
-import {
-    treeTrawlGettingFlatInputs,
-} from "../ehr-template-processor/src/template";
+import * as treeTrawlGettingFlatInputs from "../ehr-template-processor/src/template";
 import * as axios from "axios";
 
 async function getFlatProcessedTemplate(templateName) {
