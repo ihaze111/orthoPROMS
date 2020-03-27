e50d33d57f1e5fe6ce6dadfa40b97f23a1f7e6a9
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-27 16:34:50 +0000

    Fixed cookie issue

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -3,25 +3,25 @@ import environment from '../environment';
 
 export const userSignupRequest = userData => {
     return dispatch => {
-        return axios.post(environment.login_url + '/api/auth/signup', userData)
+        return axios.post(environment.login_url + '/api/auth/signup', userData, { withCredentials: true })
     }
 };
 
 export const sendCode = (data) => {
     return dispatch => {
-        return axios.post(environment.login_url + '/api/auth/code', data)
+        return axios.post(environment.login_url + '/api/auth/code', data, { withCredentials: true })
     }
 };
 
 export const checkCode = data => {
     return dispatch => {
-        return axios.post(environment.login_url + '/api/auth/checkCode', data)
+        return axios.post(environment.login_url + '/api/auth/checkCode', data, { withCredentials: true })
     }
 };
 
 export const reset = data => {
     return dispatch => {
-        return axios.post(environment.login_url + '/api/auth/reset', data)
+        return axios.post(environment.login_url + '/api/auth/reset', data, { withCredentials: true })
     }
 };
 
