8e762e4165a4fcf534bf78818ccf46446e05c4d2
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-29 00:48:52 +0000

    Fixed compositions page

--- src/views/Composition.js
+++ src/views/Composition.js
@@ -14,18 +14,26 @@ import getCompositionByCompositionId from "../components/Queries/getCompositionB
 import CDRAQLQuery from "../components/Queries/CDRAQLQuery";
 import NHSBackLink from "../components/react-styled-nhs/src/NHSBackLink";
 import { createBrowserHistory } from "history";
+import NHSCheckbox from "../components/react-styled-nhs/src/NHSCheckbox";
+import { NHSFormGroup, NHSFormLabel } from "../components/react-styled-nhs/src/NHSComponents";
 
 export class Composition extends React.Component {
     constructor(props) {
         super(props);
         this.compId = qs.parse(this.props.location.search, { ignoreQueryPrefix: true }).compId ?
             qs.parse(this.props.location.search, { ignoreQueryPrefix: true }).compId : "";
-        this.state = {};
+        this.state = {
+            showEmpty: false
+        };
     }
 
     goBack() {
         createBrowserHistory().goBack();
     }
+
+    handleChange = (e) => {
+        this.setState({ 'showAll': e.target.checked });
+    };
 
     componentDidMount() {
         let promise = getCompositionByCompositionId(this.compId);
@@ -55,12 +63,46 @@ export class Composition extends React.Component {
                             row[queryCell] = row[queryCell] === null ? queryRow[queryCell] : row[queryCell];
                         });
                     });
-                    console.log(row);
                     const final = Object.keys(row).map((cell) => {
-                        let valueToShow = JSON.stringify(row[cell]);
-                            if (row[cell] != null && row[cell] != undefined) {
-                            valueToShow = row[cell].value !== undefined && row[cell].value !== null ? row[cell].value :
-                                (row[cell].code_string ? row[cell].code_string : (row[cell].name ? row[cell].name : JSON.stringify((row[cell].terminology_id ? row[cell].terminology_id : JSON.stringify(row[cell]))))).toString();
+                        let valueToShow;
+                        if (row[cell] != null) {
+                            switch (row[cell]['@class']) {
+                                case 'DV_DATE_TIME':
+                                    valueToShow = row[cell].value;
+                                    break;
+                                case 'DV_ORDINAL':
+                                    valueToShow = <span>{row[cell].symbol.value} <span
+                                        style={{ color: 'grey' }}>({row[cell].value})</span></span>;
+                                    break;
+                                case 'DV_CODED_TEXT':
+                                    valueToShow = row[cell].value;
+                                    break;
+                                case 'CODE_PHRASE':
+                                    valueToShow = row[cell].terminology_id.code_string;
+                                    break;
+                                case 'PARTY_IDENTIFIED':
+                                    valueToShow = row[cell].name;
+                                    break;
+                                case 'DV_QUANTITY':
+                                    valueToShow = row[cell].magnitude + " " + row[cell].units;
+                                    break;
+                                case 'DV_PROPORTION':
+                                    valueToShow =
+                                        <span><sup>{row[cell].numerator}</sup>&frasl;<sub>{row[cell].denominator}</sub></span>;
+                                    break;
+                                case 'DV_TEXT':
+                                    valueToShow = row[cell].value;
+                                    break;
+                                case 'PARTY_SELF':
+                                    valueToShow = <span style={{ color: 'grey' }}>(self)</span>;
+                                    break;
+                                case 'DV_PARSABLE':
+                                    valueToShow = row[cell].value;
+                                    break;
+                                default:
+                                    valueToShow = JSON.stringify(row[cell]);
+                                    break;
+                            }
                         }
                         return [mapping[cell][1], valueToShow];
                     });
@@ -84,11 +126,19 @@ export class Composition extends React.Component {
                 <NHSContainer>
                     <NHSWrapper>
                         <NHSBackLink onClick={this.goBack}>Go back</NHSBackLink>
+                        <NHSFormGroup onChange={this.handleChange}>
+                            <NHSFormLabel>Show empty rows</NHSFormLabel>
+                            <NHSCheckbox name={'showEmpty'}/>
+                        </NHSFormGroup>
                         <NHSSummaryList style={{ width: '70%' }}>
-                            {this.state.composition.map(tableRow => <NHSSummaryListRow>
-                                    <NHSSummaryListKey>{tableRow[0]}</NHSSummaryListKey>
-                                    <NHSSummaryListValue>{tableRow[1]}</NHSSummaryListValue>
-                                </NHSSummaryListRow>
+                            {this.state.composition.map(tableRow => {
+                                    if (this.state.showAll || tableRow[1] !== undefined) {
+                                        return <NHSSummaryListRow>
+                                            <NHSSummaryListKey>{tableRow[0]}</NHSSummaryListKey>
+                                            <NHSSummaryListValue>{tableRow[1]}</NHSSummaryListValue>
+                                        </NHSSummaryListRow>
+                                    }
+                                }
                             )}
                         </NHSSummaryList>
                     </NHSWrapper>
