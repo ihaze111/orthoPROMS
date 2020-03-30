45e7941ce98d7a8d34cd09ea574c9d7ae1da2f85
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-30 18:55:14 +0100

    Fixed linting in src/components/Clinician

--- src/components/Clinician/PatientListEntry.js
+++ src/components/Clinician/PatientListEntry.js
@@ -1,34 +1,35 @@
 import React from 'react';
-import { NHSTd, NHSTr } from "../react-styled-nhs/src/NHSTableWrapperTest";
-import * as PropTypes from "prop-types";
+import * as PropTypes from 'prop-types';
+import { NHSTd, NHSTr } from '../react-styled-nhs/src/NHSTableWrapperTest';
 
 /**
- * Create an (NHS-styled) table entry for an individual patient in the list of patients that is displayed to a clinician
+ * Create an (NHS-styled) table entry for an individual patient in the list of patients that is
+ * displayed to a clinician
  * @param props
  * @returns {*}
  * @constructor
  */
 function PatientListEntry(props) {
-    return (
-        <NHSTr>
-            <NHSTd>{props.id}</NHSTd>
-            <NHSTd><a href={"PatientRecords?subjectId=" + props.subjectId}>{props.nhsNumber}</a></NHSTd>
-            <NHSTd>{props.gender}</NHSTd>
-            <NHSTd>{props.sex}</NHSTd>
-            <NHSTd>{props.vitalStatus}</NHSTd>
-            <NHSTd>{props.birthYear}</NHSTd>
-        </NHSTr>
-    );
+  return (
+    <NHSTr>
+      <NHSTd>{props.id}</NHSTd>
+      <NHSTd><a href={`PatientRecords?subjectId=${props.subjectId}`}>{props.nhsNumber}</a></NHSTd>
+      <NHSTd>{props.gender}</NHSTd>
+      <NHSTd>{props.sex}</NHSTd>
+      <NHSTd>{props.vitalStatus}</NHSTd>
+      <NHSTd>{props.birthYear}</NHSTd>
+    </NHSTr>
+  );
 }
 
 PatientListEntry.propTypes = {
-    id: PropTypes.number,
-    subjectId: PropTypes.string,
-    nhsNumber: PropTypes.string,
-    gender: PropTypes.string,
-    sex: PropTypes.string,
-    vitalStatus: PropTypes.string,
-    birthYear: PropTypes.string
+  id: PropTypes.number,
+  subjectId: PropTypes.string,
+  nhsNumber: PropTypes.string,
+  gender: PropTypes.string,
+  sex: PropTypes.string,
+  vitalStatus: PropTypes.string,
+  birthYear: PropTypes.string,
 };
 
 export default PatientListEntry;
