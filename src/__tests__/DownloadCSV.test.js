d2880e4cb870d8a4b73e85a41105abdb8d76c6e1
ihaze111 <shahrin.haziq@gmail.com>
2020-03-30 01:19:37 +0100

    Fixed test

--- src/__tests__/DownloadCSV.test.js
+++ src/__tests__/DownloadCSV.test.js
@@ -1,9 +1,9 @@
-import { TransposeFunction } from "../components/DownloadCSV";
+import { transpose } from "../components/DownloadCSV.js";
 
 it('transpose exist', () => {
-    expect(TransposeFunction.Transpose).toBeDefined();
+    expect(transpose).toBeDefined();
 });
 
 it('transpose', () => {
-    expect(TransposeFunction.Transpose([[1,2,3],[1,2,3]])).toEqual([[1,1],[2,2],[3,3]]);
+    expect(transpose([[1,2,3],[1,2,3]])).toEqual([[1,1],[2,2],[3,3]]);
 });
