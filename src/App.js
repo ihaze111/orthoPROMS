e4914639a2305f5afe348d445fc50015a7dc54bd
Charlie Cowan <charlie.g.cowan@gmail.com>
2020-03-30 20:31:10 +0100

    Fixed most linting issues in root of src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Script from 'react-load-script'
+import Script from 'react-load-script';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './views/Home';
 import PatientSelf from './views/Patient/PatientSelf';
@@ -10,55 +10,56 @@ import Register from './views/Register';
 import About from './views/About';
 import Reset from './views/Reset';
 import NationalStatistics from './views/NationalStatistics';
-import 'nhsuk-frontend/dist/nhsuk.min.js';
+import 'nhsuk-frontend/dist/nhsuk.min';
 import 'nhsuk-frontend/packages/nhsuk.scss';
-import { Composition } from "./views/Composition";
+import { Composition } from './views/Composition';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.startApp = () => {
-            let gapi = window.gapi;
-            gapi.load('auth2', () => {
-                gapi.auth2.init({
-                    client_id: '435425463824-lso8p4egc7hasvbkrbff4h60g60se5l3.apps.googleusercontent.com'
-                }).then(res => {
-                    console.log(res);
-                })
-            })
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.startApp = () => {
+      const { gapi } = window;
+      gapi.load('auth2', () => {
+        gapi.auth2.init({
+          client_id: '435425463824-lso8p4egc7hasvbkrbff4h60g60se5l3.apps.googleusercontent.com',
+        })
+          .then((res) => {
+            // TODO: replace this
+            console.log(res);
+          });
+      });
+    };
+  }
 
-    render() {
-        return (
-
-            <Router>
-                <link
-                    rel="stylesheet"
-                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                    crossOrigin="anonymous"
-                />
-                <script src="http://cdn.highcharts.com.cn/highcharts/highcharts.js"></script>
-                <Script
-                    url="https://apis.google.com/js/platform.js"
-                    onLoad={this.handleScriptLoad}
-                />
-                <Router basename={'/'}>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/Login" component={Login}/>
-                    <Route path="/About" component={About}/>
-                    <Route path="/Register" component={Register}/>
-                    <Route path="/Patient" component={PatientSelf}/>
-                    <Route path="/Composition" component={Composition}/>
-                    <Route path="/Clinician" component={PatientList}/>
-                    <Route path="/PatientRecords" component={PatientRecords}/>
-                    <Route path="/Reset" component={Reset}/>
-                    <Route path="/NationalStatistics" component={NationalStatistics}/>
-                </Router>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+        <script src="http://cdn.highcharts.com.cn/highcharts/highcharts.js" />
+        <Script
+          url="https://apis.google.com/js/platform.js"
+          onLoad={this.handleScriptLoad}
+        />
+        <Router basename="/">
+          <Route exact path="/" component={Home} />
+          <Route path="/Login" component={Login} />
+          <Route path="/About" component={About} />
+          <Route path="/Register" component={Register} />
+          <Route path="/Patient" component={PatientSelf} />
+          <Route path="/Composition" component={Composition} />
+          <Route path="/Clinician" component={PatientList} />
+          <Route path="/PatientRecords" component={PatientRecords} />
+          <Route path="/Reset" component={Reset} />
+          <Route path="/NationalStatistics" component={NationalStatistics} />
+        </Router>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -23,35 +23,36 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './reducers'
-import logger from 'redux-logger'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import jwtDecode from 'jwt-decode';
-import {setCurrentUser} from './actions/authActions'
+import rootReducer from './reducers';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import { setCurrentUser } from './actions/authActions';
 import setAuthorizationToken from './utils/setAuthorizationToken';
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
-    applyMiddleware(thunk,logger)
-  )
-)
+    applyMiddleware(thunk, logger),
+  ),
+);
 
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   setAuthorizationToken(localStorage.jwtToken);
-  store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken),JSON.parse(localStorage.getItem('isGoogleLogin'))));
+  store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken), JSON.parse(localStorage.getItem('isGoogleLogin'))));
 }
 
 ReactDOM.render(
   <Provider store={ store }>
   <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
